ece4b6c24b63911c7faa66ab75f57f5ae32dccd1
AdarshHota <adarshhota55@gmail.com>
2022-11-03 22:57:30 +0530

    feat: add state persistance feature
    add state persistance for token validation on server side

---
+++ server/middlewares/getUserDetails.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+const { PRIVATE_KEY } = require('../secrets');
+
+const getUserDetails = async (req, res, next) => {
+    try {
+        const token = req.header('x-auth-token');
+        if (!token) {
+            return res
+                .status(401)
+                .json({
+                    msg: 'No auth token, access denied'
+                });
+        }
+
+        const decodedToken = jwt.verify(token, PRIVATE_KEY);
+        if (!decodedToken) {
+            return res
+                .status(401)
+                .json({
+                    msg: 'Token verification failed, authorization denied'
+                });
+        }
+
+        req.user = decodedToken.id;
+        req.token = token;
+        next();
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ error: error.message })
+    }
+}
+
+module.exports = getUserDetails;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const authRouter = express.Router();
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/userSchema');
 const { PRIVATE_KEY } = require('../secrets');
+const getUserDetails = require('../middlewares/getUserDetails');
 
+
+const authRouter = express.Router();
+
+//api for signing up
 authRouter
     .post('/auth/signUp', async (req, res) => {
         try {
@@ -19,7 +23,6 @@ authRouter
             }
 
             const hashedPassword = await bcryptjs.hash(password, 9);
-
             let user = new User({
                 name: name,
                 email: email,
@@ -27,7 +30,6 @@ authRouter
             });
 
             user = await user.save();
-
             return res
                 .status(200)
                 .json({ user: user });
@@ -36,11 +38,11 @@ authRouter
             return res
                 .status(500)
                 .json({ error: error.message })
-
         }
 
     })
 
+//api for signing in
 authRouter
     .post('/auth/signIn', async (req, res) => {
         try {
@@ -52,27 +54,73 @@ authRouter
                     .status(400)
                     .json({ msg: 'Entered email doesn\'t exist' });
             }
-            
+
             const isMatch = await bcryptjs.compare(password, user.password);
             if (!isMatch) {
                 return res
                     .status(400)
                     .json({ msg: 'Incorrect password' });
-
             }
 
-            const token = await jwt.sign({ id: user._id }, PRIVATE_KEY);
+            const token = jwt.sign({ id: user._id }, PRIVATE_KEY);
             return res
                 .status(200)
                 .json({ token: token, ...user._doc });
-                
+
         } catch (error) {
             return res
                 .status(500)
                 .json({ error: error.message })
-
         }
 
     })
+
+//api for checking if user token is valid
+authRouter
+    .post('/auth/checkTokenValid', async (req, res) => {
+        try {
+            const token = req.header('x-auth-token');
+
+            //check if token is valid
+            const decodedToken = jwt.verify(token, PRIVATE_KEY);
+            if (!decodedToken) {
+                return res
+                    .json(false);
+            }
+
+            //check if user exists in the database
+            const user = User.findById(decodedToken.id);
+            if (!user) {
+                return res
+                    .json(false);
+            }
+
+            return res
+                .json(true);
+
+        } catch (error) {
+            return res
+                .status(500)
+                .json({ error: error.message })
+        }
+
+    })
+
+//api for getting user details 
+authRouter.get('/', getUserDetails, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id);
+        return res
+            .status(200)
+            .json({
+                ...user._doc,
+                token: req.token,
+            })
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ error: error.message })
+    }
+})
 
 module.exports = authRouter;
