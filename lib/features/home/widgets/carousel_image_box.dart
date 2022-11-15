a78d1656760cf905dddfd81497530743f4f5c8a5
AdarshHota <adarshhota55@gmail.com>
2022-11-15 17:10:16 +0530

    feat: add carousel image section

---
+++ lib/features/home/widgets/carousel_image_box.dart
@@ -0,0 +1,27 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:your_shop_app/constants/global_variables.dart';
+
+class CarouselImageBox extends StatelessWidget {
+  const CarouselImageBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+      items: GlobalVariables.carouselImages
+          .map(
+            (imageLink) => Image.network(
+              imageLink,
+              height: 200,
+         
+              fit: BoxFit.cover,
+            ),
+          )
+          .toList(),
+      options: CarouselOptions(
+        viewportFraction: 1,
+        height: 200,
+      ),
+    );
+  }
+}
--- lib/features/home/widgets/home_main_page.dart
+++ lib/features/home/widgets/home_main_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:your_shop_app/features/home/widgets/address_box.dart';
+import 'package:your_shop_app/features/home/widgets/carousel_image_box.dart';
 import 'package:your_shop_app/features/home/widgets/top_categories_box.dart';
 
 class HomeMain extends StatelessWidget {
@@ -14,6 +15,10 @@ class HomeMain extends StatelessWidget {
           height: 10,
         ),
         TopCategoriesBox(),
+        SizedBox(
+          height: 10,
+        ),
+        CarouselImageBox(),
       ],
     );
   }
